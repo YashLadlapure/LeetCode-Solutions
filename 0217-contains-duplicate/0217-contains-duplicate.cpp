ac2a685dbf9fbc1b3e5f5a6a14d0f329835ae323
Yash Ladlapure <yash.ladlapure@gmail.com>
2026-05-02 01:43:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums)
-    
- {
-        sort(nums.begin(),nums.end());
-    for(int i=1;i<nums.size();i++)
-    {
-        if(nums[i]==nums[i-1])
-        {
-            return true;
-
-        }
-       
-           
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         
-    } 
-    return false;
-  }
+        for(int i = 0; i < nums.size() - 1; i++) {  // Index-based
+            if(nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
 };
